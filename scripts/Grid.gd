fca0fc85ed6ec0e8d971b5e6a160abf5a34631b7
harold-kalvin <harold.k.basa@gmail.com>
2019-12-02 22:35:34 +0100

    populate grid with random tiles

--- scripts/Grid.gd
+++ scripts/Grid.gd
@@ -6,15 +6,16 @@ var x_start = 120
 var y_start = 1160
 var offset = 80
 var grid = []
-var tiles = [
+var possible_tiles = [
 	preload("res://scenes/tiles/CircleTile.tscn"),
 	preload("res://scenes/tiles/SquareTile.tscn"),
 	preload("res://scenes/tiles/TriangleTile.tscn"),
 ]
 
 func _ready():
+	randomize()
 	grid = init_grid()
-	print(grid)
+	populate_grid()
 
 func init_grid():
 	var array = []
@@ -24,6 +25,18 @@ func init_grid():
 			array[i].append(null)
 	return array
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func populate_grid():
+	for i in rows:
+		for j in cols:
+			# choose random tile
+			var rand = floor(rand_range(0, possible_tiles.size()))
+			var tile = possible_tiles[rand].instance()
+			# add it to grid
+			add_child(tile)
+			tile.position = get_tile_position(i, j)
+			grid[i][j] = tile
+
+func get_tile_position(row, col):
+	var x = x_start + offset * col
+	var y = y_start + -offset * row
+	return Vector2(x, y)
