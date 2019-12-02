7897448e990b51183a8ad8413abc7eb363144f20
harold-kalvin <harold.k.basa@gmail.com>
2019-12-02 22:55:24 +0100

    replace tab with 4 spaces

--- scripts/Grid.gd
+++ scripts/Grid.gd
@@ -7,36 +7,36 @@ var y_start = 1160
 var offset = 80
 var grid = []
 var possible_tiles = [
-	preload("res://scenes/tiles/CircleTile.tscn"),
-	preload("res://scenes/tiles/SquareTile.tscn"),
-	preload("res://scenes/tiles/TriangleTile.tscn"),
+    preload("res://scenes/tiles/CircleTile.tscn"),
+    preload("res://scenes/tiles/SquareTile.tscn"),
+    preload("res://scenes/tiles/TriangleTile.tscn"),
 ]
 
 func _ready():
-	randomize()
-	grid = init_grid()
-	populate_grid()
+    randomize()
+    grid = init_grid()
+    populate_grid()
 
 func init_grid():
-	var array = []
-	for i in rows:
-		array.append([])
-		for j in cols:
-			array[i].append(null)
-	return array
+    var array = []
+    for i in rows:
+        array.append([])
+        for j in cols:
+            array[i].append(null)
+    return array
 
 func populate_grid():
-	for i in rows:
-		for j in cols:
-			# choose random tile
-			var rand = floor(rand_range(0, possible_tiles.size()))
-			var tile = possible_tiles[rand].instance()
-			# add it to grid
-			add_child(tile)
-			tile.position = get_tile_position(i, j)
-			grid[i][j] = tile
+    for i in rows:
+        for j in cols:
+            # choose random tile
+            var rand = floor(rand_range(0, possible_tiles.size()))
+            var tile = possible_tiles[rand].instance()
+            # add it to grid
+            add_child(tile)
+            tile.position = get_tile_position(i, j)
+            grid[i][j] = tile
 
 func get_tile_position(row, col):
-	var x = x_start + offset * col
-	var y = y_start + -offset * row
-	return Vector2(x, y)
+    var x = x_start + offset * col
+    var y = y_start + -offset * row
+    return Vector2(x, y)
