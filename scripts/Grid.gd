a29186225475d2b9cb2a0cd1477825ff72956751
harold-kalvin <harold.k.basa@gmail.com>
2019-12-02 23:26:29 +0100

    make sure 3 tiles of the same type are not aligned on start

--- scripts/Grid.gd
+++ scripts/Grid.gd
@@ -12,10 +12,12 @@ var possible_tiles = [
     preload("res://scenes/tiles/TriangleTile.tscn"),
 ]
 
+
 func _ready():
     randomize()
     grid = init_grid()
     populate_grid()
+
 
 func init_grid():
     var array = []
@@ -25,16 +27,33 @@ func init_grid():
             array[i].append(null)
     return array
 
+
 func populate_grid():
     for i in rows:
         for j in cols:
-            # choose random tile
-            var rand = floor(rand_range(0, possible_tiles.size()))
-            var tile = possible_tiles[rand].instance()
+            # generate random tile and make sure 3 tiles of the same type are not aligned
+            var tile = get_random_tile()
+            while chain_exists(i, j, tile.type):
+                tile = get_random_tile()
+            
             # add it to grid
             add_child(tile)
             tile.position = get_tile_position(i, j)
             grid[i][j] = tile
+
+
+func get_random_tile():
+    var rand = floor(rand_range(0, possible_tiles.size()))
+    return possible_tiles[rand].instance()
+
+
+func chain_exists(row, col, type):
+    if row > 1 && grid[row - 1][col].type == type && grid[row - 2][col].type == type:
+        return true
+    if col > 1 && grid[row][col - 1].type == type && grid[row][col - 2].type == type:
+        return true
+    return false
+
 
 func get_tile_position(row, col):
     var x = x_start + offset * col
